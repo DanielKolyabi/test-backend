8e9e996655b540c5c0c979be4ef451d9486a4227
daniil.kolyabin <kolabin14@gmail.com>
2025-01-14 00:58:57 +0300

    fix budget stats sort order

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -2,25 +2,20 @@ package mobi.sevenwinds.app.budget
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import mobi.sevenwinds.app.author.AuthorEntity
-import mobi.sevenwinds.app.author.AuthorTable
-import mobi.sevenwinds.app.ilike
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.sum
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object BudgetService {
-    suspend fun addRecord(body: BudgetRecordCreateRequest): BudgetRecord = withContext(Dispatchers.IO) {
+    suspend fun addRecord(body: BudgetRecord): BudgetRecord = withContext(Dispatchers.IO) {
         transaction {
-            val author = body.authorId?.let { id ->
-                AuthorEntity.findById(id) ?: throw IllegalArgumentException("Автор id=$id не существует")
-            }
             val entity = BudgetEntity.new {
                 this.year = body.year
                 this.month = body.month
                 this.amount = body.amount
                 this.type = body.type
-                this.author = author
             }
 
             return@transaction entity.toResponse()
@@ -29,25 +24,20 @@ object BudgetService {
 
     suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
         transaction {
-            val withBudgetRecordFilter = buildFilterFrom(param)
-            val tables = BudgetTable.leftJoin(AuthorTable)
-
-            val itemsQuery = tables
-                .select { withBudgetRecordFilter }
+            val withBudgetYear = BudgetTable.year eq param.year
+            val itemsQuery = BudgetTable
+                .select { BudgetTable.year eq param.year }
                 .orderBy(BudgetTable.month, SortOrder.ASC)
                 .orderBy(BudgetTable.amount, SortOrder.DESC)
                 .limit(param.limit, param.offset)
 
             val data = BudgetEntity.wrapRows(itemsQuery).map { it.toResponse() }
 
-            val total = tables
-                .select { withBudgetRecordFilter }
-                .count()
+            val total = BudgetTable.select { withBudgetYear }.count()
 
             val sum = BudgetTable.amount.sum()
-            val sumByType = tables
-                .slice(BudgetTable.type, sum)
-                .select { withBudgetRecordFilter }
+            val sumByType = BudgetTable.slice(BudgetTable.type, sum)
+                .select { withBudgetYear }
                 .groupBy(BudgetTable.type)
                 .associate { resultRow -> resultRow[BudgetTable.type].name to (resultRow[sum] ?: 0) }
 
@@ -56,17 +46,6 @@ object BudgetService {
                 totalByType = sumByType,
                 items = data
             )
-        }
-    }
-
-    private fun buildFilterFrom(param: BudgetYearParam): Op<Boolean> {
-        val yearFilter = BudgetTable.year eq param.year
-
-        return if (param.authorName != null) {
-            val authorNameFilter = AuthorTable.fullName.ilike("%${param.authorName}%")
-            yearFilter and authorNameFilter
-        } else {
-            yearFilter
         }
     }
 }
